7d6266124be93e901838c9d828e0cd5b1875577b
Arijit Bardhan <baarijit.bardhan@hotmail.co.in>
2025-02-21 16:06:27 +0530

    added key pair and auth variables

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,6 +11,8 @@ terraform {
 
 provider "aws" {
   region  = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 # for getting AMI id
@@ -30,6 +32,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "app_server_instance_key" {
+  key_name   = "AppServer_Instance_Key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDjjRLbp1E33tjqSgvJdPy0d0O0skPuUuUHg98u4EDoYR5J1B5MLjiTao1AAtdngilUxHv7bXHbVSJCEbH9/6+Xs3gug8TThhuNfqcDt4r/SxKVshiaDd/iK1pFFo0YxXc47n0111zuanhKoDt/pUCrJ1h84lfoBEp7cUBnlrYa5vi1qApMOffHmlOwdMihJhIVXq+Y0br7rRMirAgfZtTn8Qeg3xnxRtrrFmKIdvvegnY86pTHCAnqiYWWytbKEazXopAJcR+kT6MHLEsco/w7ImP/3pu8yIqQYjjvD8ynyw17L663AIryuEPG4a623xLcKsR4GdTZl5mAwJpmnQBrIYfOhziW6EXNXCVUUbFfVPtMMtJa+xHqAmBYYKe/t71Afa0iB35liP+lYGJ+IsM980JbGAkQLQWPEr/iv67hhhMv8FlAsnVJciGwenWYDGZ0wR8vUrHrmAOdsnynFhUP9etuOn+s6iPAL40VzUrqRlUdX8XjmQg2Qr2a5zPbNV/x/+I7NIZk3RLQVFlHNSdOOsl6tNYYjJE+n4s0oejlBG+sxS53IJrHgCfmKIPkl9M0pS/BjTix3fg8OeFzmXKIuAO53dObvbD7WO2DH/cASpHLlyxfiH/sbiYmqqonKhEzJL3wb54FrwpETQ3P6utFXedcWLh0/lpN/HSF7d0E9Q== ubuntu@ip-172-31-46-136"
+}
+
 resource "aws_instance" "app_server_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -39,6 +46,8 @@ resource "aws_instance" "app_server_instance" {
     Name = "AppServer_Instance"
     
   }
+  key_name = aws_key_pair.app_server_instance_key.key_name
+
   # defining storage
   root_block_device {
     delete_on_termination = true
