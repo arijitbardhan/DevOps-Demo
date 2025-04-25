bc97eac390c1ae6f700660ae880b69018feff657
Arijit Bardhan <baarijit.bardhan@hotmail.co.in>
2025-04-25 18:51:18 +0530

    reduced host number in VPC CIDR notation

--- Terraform/aws/variables.tf
+++ Terraform/aws/variables.tf
@@ -59,7 +59,7 @@ variable "ebs_attachment_to_ec2_device_name" {
 
 variable "app_server_vpc_cidr_block" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/8"
 }
 
 variable "app_server_subnet_cidr_block" {
